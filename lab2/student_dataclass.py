28493c447a101d06c0d0fcdb157a8f1d99961e21
Fernando Molano <fm87usa@gmail.com>
2021-08-28 16:09:11 -0500

    Override str method to improve printed info readability

--- lab2/student_dataclass.py
+++ lab2/student_dataclass.py
@@ -7,6 +7,9 @@ class Student:
     college_id: int
     gpa: float
 
+    def __str__(self):
+        return f'Student: {self.name}\nCollege id: {self.college_id}\nGPA: {self.gpa}\n'
+
 
 def main():
     alice = Student('Alice Cooper', 12345, 4.0)
