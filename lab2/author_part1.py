efc1ac9e7a6ed4acaa7eaea1ecbf66e14e43174a
Fernando Molano <fm87usa@gmail.com>
2021-08-28 15:43:45 -0500

    Class definition modified, no used imports deleted, comments added

--- lab2/author_part1.py
+++ lab2/author_part1.py
@@ -1,34 +1,37 @@
 """
 Author: Fernando Molano
+
+Program to define an Author class and some basic methods to manage their published books
+Each Author instance will have an empty list of books by default.
 """
 
-from dataclasses import dataclass
-from typing import List
 
-
-@dataclass
 class Author:
     def __init__(self, name):
         self.name = name
-        self.books = []
+        self.books = []  # empty list of published books by default
 
     def published(self, title):
-        self.books.append(title)
+        self.books.append(title)  # add a book title to the books list
 
     def __str__(self):
+        # separate each book title by comma or print default message if no books on list
         titles = ', '.join(self.books) or 'No published books'
         return f'{self.name}. Books: {titles}'
 
 
 def main():
+    # Create instances of Author
     marquez = Author("Gabriel Garcia Marquez")
     brown = Author("Dan Brown")
     rowling = Author("J.K. Rowling")
 
+    # Add books to some authors
     marquez.published("Cien años de soledad")
     marquez.published("El coronel no tiene quien le escriba")
     rowling.published("The Cuckoo's Calling")
 
+    # Show authors names and their books if any
     print(marquez)
     print(brown)
     print(rowling)
