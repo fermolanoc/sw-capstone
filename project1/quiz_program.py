5a71f73a649e89369414fa2738901a7d47c55fb3
Clara <10088152+claraj@users.noreply.github.com>
2021-09-14 10:51:44 -0500

    Update quiz_program.py
    Looks much better! More organization in the data, and in the code.    ID values are very often a good thing to add to data in your program, in the same way that primary key fields are almost always a good thing to add to a database table.  
    
    Some notes and feedback.  This isn't a working version - I didn't edit the space category, only the art category.  You don't need to merge this, it's just easier to add notes on your code this way.  
    
    The data structure is making things more difficult when you access data, and there's possible bugs that might happen - what if the IDs are not sequential, or they don't start at 1?  Converting strings to ints is also a possible source of bugs.   Would there be a better way to structure this, that makes it easier to access the data?  
    
    Suggested edits to the data structure - questions key does relate to answers value, but this makes more work when you read the question. If a dictionary only has one key-value pair, restructuring or using a different data structure may be better. 
    
    Think about names - make sure they accurately reflect what your function does. 
    
    Functions should have one specific task - processing data and printing are two different things. 
    
    Code shouldn't be in a loop if it will always happen exactly one time. 
    
    Please let me know if you have any questions on any of this. Thank you!

--- project1/quiz_program.py
+++ project1/quiz_program.py
@@ -5,23 +5,38 @@ This program will present user categories to choose. Each category will contain 
 When user answer each question, program will let user know if answer was right or wrong immediately.
 
 A score will be kept and when all the answers have been answered, total score will be shown.
- """
+"""
 
 
 # Structure with list of topics data. Each list item contain a dictionary with a topic details
+# Adding structure, and id values, is a good idea.
 topics = [{
     'id': '1',
     'name': 'art',
     'questions': [
         {
-            'Who painted the Mona Lisa?': 'Leonardo Da Vinci'
+            # 'Who painted the Mona Lisa?': 'Leonardo Da Vinci'
+
+            # A question clearly relates to the answer, so question keys and answer values make sense. 
+            # But in this dictionary, there will only ever be one key-value pair
+            # so, its not using the capabilities of a dictionary, and you have more work in get_questions where you loop over the dictionary keys. 
+            # How about the same keys in each dictionary, to label questions and answers?  
+            # More descriptive, labels what each string is, and also would allow future expansion to store more data about each question,
+            # for example custom points per question, difficulty levels etc. 
+
+            'question': 'Who painted the Mona Lisa?',
+            'answer': 'Leonardo Da Vinci',
+            # 'difficulty': 'moderate',
+            # 'points': 1           
+            
         },
         {
-            'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli'
+            'question': 'What precious stone is used to make the artist\'s pigment ultramarine?',
+            'answer': 'Lapiz lazuli'
         },
-        {
-            'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?': 'Chicago'
-        }
+        # {
+        #     'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?': 'Chicago'
+        # }
     ]
 },
     {
@@ -42,45 +57,66 @@ topics = [{
 
 
 # function to get the list of categories indexes
-def get_categories():
+# This function does two things - make a list of IDs and print them to the user. 
+# Either make the name more specific, or (better) this function should focus on one task.
+def get_category_ids():  # be clear this is returning the IDs, not the names 
     category_ids = []
 
     # loop thru the topics list
     for topic in topics:
-        print(f"\tPress {topic['id']} for {topic['name'].title()}")
+        # printing here makes this function do two things. 
+        # print(f"\tPress {topic['id']} for {topic['name'].title()}")
         # add topic id to the array category_ids
         category_ids.append(topic['id'])
 
+    # The list comprehension way - optional alternative to the loop 
+    # category_ids = [topic['id'] for topic in topics]
     return category_ids
 
 
 # function to show user questions corresponding to the category chosen. Score will be updated here
-def get_questions(category_id, score):
-    # get name of category user chose
-    category_name = topics[int(category_id) - 1]['name']
+# does this fetch and return questions? It looks like it's asking the questions - would this be a better name?
+def ask_questions(questions, category_name):  
+    # would it be easier to pass the questions list (list of Q:A dictionaries) here and the category/topic name? Less work for the function and it
+    # doesn't need to handle subtracting 1 from the category ID to find the correct questions
+    
+    # Calculate the score from 0.
+    # The function can return the score from the list of questions provided, the caller can deal with adding it onto any existing score. 
+    # This function shouldn't make assumptions about how the score is used. What if you want to display score by category? 
+    score = 0
+
+    # Pass the name as an argument  
     print(f'\nOk, you chose {category_name.upper()}. Here are the questions: ')
 
-    # get the list of questions existing on chosen category
-    questions = topics[int(category_id) - 1]['questions']
-    for item in questions:
-        for question in item.keys():  # get only the key for each question which is the question itself
-            user_answer = input(f'{question} ')
+    # get the list of questions existing on chosen category - pass this in as an argument
+    # questions = topics[int(category_id) - 1]['questions']   # this seems like extra work for this function
+    print(questions)
+    for item in questions:  # can you use a more specific name?
+        # for question in item.keys():  # get only the key for each question which is the question itself
+        question = item['question']
+        answer = item['answer']       
 
-            # if user's answer is equal to the value assigned to the question (key), increase score by 1
-            if user_answer.lower() == item[question].lower():
-                print('Correct\n')
-                score += 1
-            else:
-                print(f'No. Answer is {item[question]}\n')
+        user_answer = input(f'{question} ')
 
-    return category_name, score
+        # if user's answer is equal to the value assigned to the question (key), increase score by 1
+        if user_answer.lower() == answer.lower():
+            print('Correct\n')
+            score += 1
+        else:
+            print(f'No. Answer is {answer}\n')
+
+    return score
 
 
+# You don't need this feature. 
+# A future version of this program may contact an API for the questions, or read from a database; to set the topics data structure. 
+# And of course, the rest of your program would work without modification in that case. 
 def add_topics(number_of_topics_to_add):
 
     for i in range(number_of_topics_to_add):
         # get id of last topic currently existing on topics list
-        last_topic = int(topics[-1]['id'])
+        # the -1 and +1 that you need to do to offset the numbers is putting you at risk of off-by-one errors if you forget, or off-by-two errors if you offset the wrong wat
+        last_topic = int(topics[-1]['id'])   
         topic_name = input('Enter name of topic/category: ')
 
         topics.append({
@@ -126,25 +162,50 @@ def main():
     print('Let\'s play Trivia. Choose one of the following categories, answer right as many questions as possible and prove how smart you are:')
 
     # show category id's and names to user to choose and get list of category ids
-    category_ids = get_categories()
+    category_ids = get_category_ids()
+
+    # TODO print the topic choices here or call a method to print the choices  
+    # Displaying the choices, getting and validating the user input could be a separate function 
 
     user_choice = input('Selection: ')  # get user selection
     valid_option = False  # flag to validate user selection
 
     while not valid_option:
+
         # if user chose a number that exist on the category_ids list, raise flag to stop loop, and read questions for that category
         if user_choice in category_ids:
-            valid_option = True
-            # call get_questions method passing category user chose and score which initially is 0.
-            # Category questions will be asked and score will be updated and returned as well as the category name
-            category, score = get_questions(user_choice, total_score)
+
+            # # Code inside a loop is there because at some point, you may need to repeat it. 
+            # # This code executes one time so put it after the loop. 
+            # topic_name = '' # TODO figure this out here, look up in data strucure 
+            # valid_option = True
+            # # call get_questions method passing category user chose and score which initially is 0.
+            # # Category questions will be asked and score will be updated and returned as well as the category name
+            # score = ask_questions(user_choice, topic_name)
+
             break
         else:
             print('That is not a valid topic')
             user_choice = input('Select a category number: ')
 
+    # Move to after the loop - clearer that this happens one time. If the loop ends, the user has made valid choice.
+    category = '' # TODO figure this out here, look up in data strucure 
+    # call get_questions method passing category user chose and score which initially is 0.
+    # Category questions will be asked and score will be updated and returned as well as the category name
+
+    # Get the questions from the question bank here. 
+    questions = topics[int(user_choice) - 1]['questions']
+    score = ask_questions(questions, category)
+
+    # Add score to total 
+    total_score = total_score + score 
+
     # print results
-    number_of_questions = len(topics[int(user_choice) - 1]['questions'])
+    number_of_questions = len(topics[int(user_choice) - 1]['questions'])  # this is a complex line - is there a simpler way to express this? 
+    # converting to int, having to subtract 1 to get the right questions, these are both error-prone.
+    # What happens if the question IDs don't start at 1, or are not sequential?
+    # Think about modifications to your data structure to make it easier to read the data you need. 
+
     print(
         f"Your total score on category {category.upper()} is {score} out of {number_of_questions}")
 
